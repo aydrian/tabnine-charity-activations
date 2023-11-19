a21ec2b6e618da7309f6e8d161ef5b1e9ad4f8eb
aydrian <aydrian@gmail.com>
2023-11-19 18:25:35 -0500

    chore: added seed translations scripts for en and ja

--- prisma/sql/create_changefeed_prod.sql
+++ prisma/sql/create_changefeed_prod.sql
@@ -1,4 +1,4 @@
-CREATE CHANGEFEED INTO 'webhook-https://crl-charity-activations.fly.dev/resources/crl-cdc-webhook?insecure_tls_skip_verify=true' AS
+CREATE CHANGEFEED INTO 'webhook-https://tabnine-charity-activations.fly.dev/resources/crl-cdc-webhook?insecure_tls_skip_verify=true' AS
 SELECT
   charity_id,
   event_id,
